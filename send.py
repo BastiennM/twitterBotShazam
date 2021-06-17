a7a184f1af474dd05833e72e3b5caac7a9570329
Bastien <bastienmetais79@gmail.com>
2021-06-17 15:06:45 +0200

    remove files

--- send.py
+++
@@ -1,3 +0,0 @@
-from subprocess import call
-cmd = 'scp /home/pi/botShazam/twitterBotShazam/datanew.json u105060309@access875183491.webspace-data.io:/kunden/homepages/16/d875183491/htdocs/shazambot'
-call(cmd.split())
